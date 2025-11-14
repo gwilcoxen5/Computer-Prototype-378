9d087c3af068d30c6257eb472d136a96d44ed659
hrenteria1 <hrenteria2@unl.edu>
2025-11-14 10:19:43 -0600

    User Details UI/UX

--- client/src/Pages/UserDetail.jsx
+++ client/src/Pages/UserDetail.jsx
@@ -1,46 +1,108 @@
-import React, { useState } from 'react'
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../Styling/UserDetail.css';
+
 
 const userDetail = () => {
-    const[goal, setGoal] = useState();
+    const[userGoal, setUserGoal] = useState("");
     const[currentWeight, setCurrentWeight] = useState();
     const[desiredWeight, setDesiredWeight] = useState();
-    const[timeInterval, setTimeInterval] = useState();
+    const[timeInterval, setTimeInterval] = useState({amount: 0, unit: 'weeks',});
+    const[activeButton, setActiveButton] = useState(null);
 
     const navigate = useNavigate();
 
-    // Logic for taking user inpit for account would go here
+    const handleAmountPrompt = (e) => {
+        const amount = Number(e.target.value);
+            setTimeInterval((prev) => ({
+                ...prev,
+                amount,
+            }));
+    
+    };
+        
+        const handleUnitChange = (e) => {
+            const unit = e.target.value;
+            setTimeInterval((prev) => ({
+                ...prev,
+                unit,
+            }));
+        };
+
+
+    // Logic for taking user input for account would go here
 
     return (
-        <main>
+        <main className="general-background-container">
             <h1 className="page-title">Set Your Meal Plan Goals</h1>
+            <h2 className="goal-choice-prompt">Set Overall Goal:</h2>
+            <div>
+            <h3 className='selected-goal-display'>Selected Goal:</h3>
+            <p className='goal-choice'>{userGoal|| "None Selected yet"}</p>
+            </div>
+
+            <div className='goal-choice-container'>
+                <button className={`weight-gain-btn ${ userGoal === "Weight Gain" ? "clicked" : "" }`} onClick={() => setUserGoal("Weight Gain")}>Weight Gain</button>
+                <button className={`weight-maintain-btn ${ userGoal === "Weight Maintain" ? "clicked" : "" }`} onClick={() => setUserGoal("Weight Maintain")}>Weight Maintain</button> 
+                <button className={`weight-loss-btn ${userGoal === "Weight Loss" ? "clicked" : "" }`} onClick={() => setUserGoal("Weight Loss")}>Weight Loss</button>
+            </div>
+
+            <div className="info-form">
             <div>
                 <label className="current-weight-prompt">Set your current weight</label>
                    <input
-                    type="currentWeight"
+                    type="number"
                     id="currentWeight"
                     value={currentWeight}
-                    onChange={(e) => setCurrentWeight(e.target.value)}
+                    onChange={(e) => setCurrentWeight(Number(e.target.value))}
+                    placeholder='Ex: 100'
                 />
+                <span className='tail-text'> lbs</span>
             </div>
             <div>
-                <label>Set your desired weight</label>
+                <label className="desired-weight-prompt">Set your desired weight</label>
                     <input
-                    type="desiredWeight"
+                    type="number"
                     id="desiredWeight"
                     value={desiredWeight}
-                    onChange={(e) => setDesiredWeight(e.target.value)}
+                    onChange={(e) => setDesiredWeight(Number(e.target.value))}
+                    placeholder='Ex: 100'
                 />
+                <span className='tail-text'> lbs</span>
             </div>
-            <div>
-                <label>Set desired time intverval (weeks)</label>
-                    {/* Potential drop down menu for time interval */}
 
+            <div>
+                <label className="time-interval-prompt">Set desired time intverval (weeks)</label>
+                <div className="time-interval-dropdown">
+                    {/* Potential drop down menu for time interval */}
+                    {/* 0-100 drop down for now (could change later)*/}
+                    <select 
+                        className="time-interval-select"
+                        value={timeInterval.amount}
+                        onChange={handleAmountPrompt}
+                        >
+                            {Array.from({ length: 32 }, (_, i) => (
+                                <option key={i} value={i}>
+                                    {i}
+                                </option>
+                            ))}
+                        </select>
+
+                        {/* Unit of Time Drop Down */}
+                        <select
+                            className='time-interval-select'
+                            value={timeInterval.unit}
+                            onChange={handleUnitChange}
+                        >
+                            <option value="weeks">Weeks</option>
+                            <option value="months">Months</option>
+                            <option value="years">Years</option>
+                        </select>
+                </div>
             </div>
             <div>
-                <label>Difficulty: </label>
-                    {/* Logic to determine difficulty would go here */}
+                {/* Logic to determine difficulty would go here */}
+            </div>
             </div>
             {/* Navigation Buttons */}
             <button className="back-goal-btn" onClick={() => navigate("/myGoals")} >Back to Goal</button>
