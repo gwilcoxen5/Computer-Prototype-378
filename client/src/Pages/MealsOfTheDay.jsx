4f2c2672cc2005950affb700d77a2f93bee13979
Josiah Welch <jwelch11@huskers.unl.edu>
2025-11-16 00:36:28 -0600

    added meals list to MealsOfTheDay

--- client/src/Pages/MealsOfTheDay.jsx
+++ client/src/Pages/MealsOfTheDay.jsx
@@ -17,6 +17,12 @@ const MealsOfTheDay = () => {
         <main>
             <h1 className='meal-of-day-title'>Meals of the Day</h1>
         {/* Logic to show different types of food on button click goes here */}
+		<ul>
+			{rows.map(row => (
+				<li key={row.id}>
+					<strong>{row.name}</strong> - {row.dietary}
+				</li>
+			))}
         <div>
             <button className='breakfast-btn'>Breakfast</button>
             <button className='lunch-btn'>Lunch</button>
