a72164f6a07e28d6f6db07bcc53a9cbc0af5ff3b
Josiah Welch <jwelch11@huskers.unl.edu>
2025-11-16 00:56:58 -0600

    copied over logic to display all meals

--- client/src/Pages/PopularMealsWeek.jsx
+++ client/src/Pages/PopularMealsWeek.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios';
 
@@ -6,13 +6,28 @@ const PopularMealsWeek =() => {
 
     const navigate = useNavigate();
 
-    // Logic to 'only show meaks that fit plan needs to be implemented
+	//TODO: Get only meals that fit plan
+	const [rows, setRows] = useState([]);
+
+	useEffect(() => {
+		fetch("http://localhost:4000/api/config")
+			.then(res => res.json())
+			.then(data => setRows(data))
+			.catch(console.error);
+	}, []);
+
     return (
     <main>
         <h1 className='page-title'>Popular Meals This Week</h1>
             <div>
                 <button className='meals-for-plan-btn'>Only Show Meals That Fit With Meal Plan</button>
-                {/*Logic to display meals should go here:*/}
+		<ul>
+			{rows.map(row => (
+				<li key={row.id}>
+					<strong>{row.name}</strong> - {row.dietary}
+				</li>
+			))}
+		</ul>
             </div>
                 <button classsName='back-page-btn'onClick={() => navigate("/mealsOfDay")}>Back to Meals of the Day</button>
             <div>
