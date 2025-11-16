a1a0c016d1606f8b1ca4702209492e1bb88fb6c4
Josiah Welch <jwelch11@huskers.unl.edu>
2025-11-16 01:37:06 -0600

    fixed typo and removed error handling segment

--- client/src/Pages/Goals.jsx
+++ client/src/Pages/Goals.jsx
@@ -22,8 +22,7 @@ const Goals = () => {
 	useEffect(() => {
 		fetch("http://localhost:4000/api/config/1")
 			.then(res => res.json())
-			.then(data => setRows(data))
-			.catch(console.error);
+			.then(data => setRow(data))
 	}, []);
 
 return ( 
